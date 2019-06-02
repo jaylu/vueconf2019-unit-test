21f2bab0123bb775ef38ef02c40d5bed28434c1d
jaylu <jayjjlu@gmail.com>
2019-06-02 23:47:45 +0800

    update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -20,8 +20,7 @@ module.exports = {
     'jest-serializer-vue'
   ],
   testMatch: [
-    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
-    '**/*.spec.(js|jsx|ts|tsx)'
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)|**/*.spec.(js|jsx|ts|tsx)'
   ],
   testURL: 'http://localhost/',
   watchPlugins: [
